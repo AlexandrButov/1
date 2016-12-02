700b11c804dd04efd3ed59ee144064d94b5518d6
AlexandrButov <alexandrbutov@mail.ru>
2016-12-02 13:28:01 +0300

    develop

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -46,7 +46,7 @@
             // 
             this.button2.Location = new System.Drawing.Point(109, 13);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(75, 33);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
